1d2b8d9840412be443db53c41354908356c46605
Danissimo <danstolyarov79@gmail.com>
2024-10-20 20:26:27 +0300

    before adding interactivity

---
+++ med_app/med_app/lib/components/profile_menu_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ProfileMenuItem extends StatelessWidget {
+  ProfileMenuItem({
+    super.key,
+    required this.iconAssetPath,
+    required this.label
+  });
+  String iconAssetPath;
+  String label;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 64,
+      width: double.infinity,
+      child: Row(
+        children: [
+          Image.asset(iconAssetPath),
+          SizedBox(width: 20,),
+          Text(label, style: TextStyle(
+            fontFamily: "Montserrat",
+            fontSize: 17,
+            height: 24/17,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0
+          ),)
+        ],
+      ),
+    );
+  }
+}
--- med_app/med_app/lib/pages/profile.dart
+++ med_app/med_app/lib/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:med_app/components/profile_menu_item.dart';
 class ProfilePage extends StatefulWidget{
   ProfilePage({super.key});
   @override
@@ -13,6 +14,107 @@ class _ProfilePageState extends State<ProfilePage> {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Theme.of(context).canvasColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 27, top: 92),
+            child: const Text(
+                "Эдуард",
+                style: TextStyle(
+                    fontFamily: "Montserrat",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
+                    height: 28 / 24,
+                    letterSpacing: 0.33),
+              ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 27, top: 22),
+            child: const Text(
+                "+7 900 800-55-33",
+                style: TextStyle(
+                    fontFamily: "Montserrat",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                    height: 16 / 15,
+                    color: Color(0xFF898A8D),
+                    letterSpacing: 0.33),
+              ),
+          ),Container(
+            margin: const EdgeInsets.only(left: 27, top: 16),
+            child: const Text(
+                "email@gmail.com",
+                style: TextStyle(
+                    fontFamily: "Montserrat",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                    height: 16 / 15,
+                    color: Color(0xFF898A8D),
+                    letterSpacing: 0.33),
+              ),
+          ),
+          SizedBox(height: 48,),
+          Container(
+            padding: EdgeInsets.only(left: 27, right: 28),
+            child: Column(
+              children: [
+                ProfileMenuItem(label: "Мои заказы", iconAssetPath: "assets/icons/order.png"),
+                ProfileMenuItem(label: "Медицинские карты", iconAssetPath: "assets/icons/cards.png"),
+                ProfileMenuItem(label: "Мои адреса", iconAssetPath: "assets/icons/adress.png"),
+                ProfileMenuItem(label: "Настройки", iconAssetPath: "assets/icons/settings.png"),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ProfileBottomMenuItem(label: "Ответы на вопросы"),
+                      ProfileBottomMenuItem(label: "Политика конфиденциальности"),
+                      ProfileBottomMenuItem(label: "Пользовательское соглашение"),
+                      ProfileBottomMenuItem(label: "Выход",color: Color(0xFFFD3535),),
+                
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileBottomMenuItem extends StatelessWidget {
+  ProfileBottomMenuItem({
+    super.key,
+    required this.label,
+    this.color
+  });
+  String label;
+  Color? color;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        foregroundColor: color ?? Color(0xFF939396),
+        textStyle: TextStyle(
+          fontFamily: "Montserrat",
+          fontSize: 15,
+          height: 20/15,
+          letterSpacing: 0,
+        )
+      ),
+      onPressed: () {},
+      child: Text(label),
     );
   }
 }
